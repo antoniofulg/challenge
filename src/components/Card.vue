5ea599719c0f953fde2aa8d4ac8b4f5f29dc4939
Antonio Fulgencio <antoniofulg@gmail.com>
2020-06-12 18:26:32 -0300

    Card componentizado

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card rounded-0 shadow-sm">
+        <div
+            class="card-body"
+            :class="{'py-5': padding}"
+        >
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['padding']
+    }
+</script>
